ede0f50ea62539dfba2b09be721cad6935227674
Brice <brice_wen@myplay20.com>
2020-07-10 09:51:36 +0800

    Add manifest tp sw

--- src/sw-precache-config.js
+++ src/sw-precache-config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  staticFileGlobs: ["index.html", "./style/style.css", "./assets/images/**.*"],
+  staticFileGlobs: [
+    "index.html",
+    "manifest.json",
+    "./style/style.css",
+    "./assets/images/**.*",
+  ],
   runtimeCaching: [
     {
       urlPattern: /this\\.is\\.a\\.regex/,
